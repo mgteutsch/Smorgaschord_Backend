4d31231567567a1a4c14c011c646b22006e67a0d
mgteutsch <mgt0117@gmail.com>
2017-06-06 20:41:22 -0500

    Login and Signup functionality working

--- Smorgaschord_Backend/App_xSmorgaschordx/smorgaschordApp.js
+++ Smorgaschord_Backend/App_xSmorgaschordx/smorgaschordApp.js
@@ -1,1 +1,36 @@
-﻿console.log("Hello there");
+﻿var app = angular.module("Smorgaschord", ["ngRoute"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when("/",
+        {
+            templateUrl: "App_xSmorgaschordx/Partials/login.html",
+            controller: "LoginController"
+        })
+        .when("/home",
+        {
+            templateUrl: "App_xSmorgaschordx/Partials/home.html",
+            controller: "HomeController"
+        })
+        .when("/signup",
+        {
+            templateUrl: "App_xSmorgaschordx/Partials/signup.html",
+            controller: "SignupController"
+        })
+        .when("/login",
+        {
+            templateUrl: "App_xSmorgaschordx/Partials/Login.html",
+            controller: "LoginController"
+        })
+        .otherwise("/login");
+}])
+
+
+app.run(["$http", function ($http) {
+
+    var token = sessionStorage.getItem('token');
+
+    if (token)
+        $http.defaults.headers.common['Authorization'] = `bearer ${token}`;
+
+    }
+]);
