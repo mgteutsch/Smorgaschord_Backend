99cf32a900e258af8468e8e037ad0e9e700618b4
mgteutsch <mgt0117@gmail.com>
2017-06-10 13:43:46 -0500

    ChordComposer Model created and added to IdentityModel

--- Smorgaschord_Backend/Models/IdentityModels.cs
+++ Smorgaschord_Backend/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
 
 namespace Smorgaschord_Backend.Models
 {
@@ -25,6 +26,8 @@ namespace Smorgaschord_Backend.Models
         {
         }
         
+        public DbSet<ChordComposer> ChordComposerProgression { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
