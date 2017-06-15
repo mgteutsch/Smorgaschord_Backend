2aefc56605a21216a8e71825605da9e118363d67
mgteutsch <mgt0117@gmail.com>
2017-06-15 15:51:50 -0500

    Added migration and can POST lesson title to initialize

--- Smorgaschord_Backend/App_xSmorgaschordx/Controllers/CustomLessonContainerCtrl.js
+++ Smorgaschord_Backend/App_xSmorgaschordx/Controllers/CustomLessonContainerCtrl.js
@@ -1,13 +1,9 @@
 ﻿app.controller("CustomLessonContainerCtrl", ['$q', '$scope', '$rootScope', '$http', '$location', function ($q, $scope, $rootScope, $http, $location) {
 
-    console.log("CustomLessonCtrl is here");
-
-    $scope.showTextEditor = function () {
-        console.log("Text editor!");
-    }
-
-    $scope.showSongEditor = function () {
-        console.log("Song editor!");
-    }
-
+    $scope.saveLessonTitle = function (userTitle) {
+        $http.post('/api/CustomLessonTitle', userTitle)
+            .then(function () {
+                $scope.customLessonInitialize = {};
+            });
+        }
 }]);
---
+++ Smorgaschord_Backend/Controllers/CustomLessonContainerController.cs
@@ -0,0 +1,28 @@
+﻿using Smorgaschord_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Smorgaschord_Backend.Controllers
+{
+    public class CustomLessonContainerController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomLessonContainerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Route("api/CustomLessonTitle")]
+        [HttpPost]
+        public void AddCustomLessonTitle(CustomLessonContainer newTitle)
+        {
+            _context.CustomLessonContainers.Add(newTitle);
+            _context.SaveChanges();
+        }
+    }
+}
