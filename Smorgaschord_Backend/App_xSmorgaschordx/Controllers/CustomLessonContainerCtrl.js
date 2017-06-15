456edd7ef9d4292ad5e547685eecdbd25364ae88
mgteutsch <mgt0117@gmail.com>
2017-06-14 23:29:48 -0500

    CustomLessons started

---
+++ Smorgaschord_Backend/App_xSmorgaschordx/Controllers/CustomLessonContainerCtrl.js
@@ -0,0 +1,13 @@
+﻿app.controller("CustomLessonContainerCtrl", ['$q', '$scope', '$rootScope', '$http', '$location', function ($q, $scope, $rootScope, $http, $location) {
+
+    console.log("CustomLessonCtrl is here");
+
+    $scope.showTextEditor = function () {
+        console.log("Text editor!");
+    }
+
+    $scope.showSongEditor = function () {
+        console.log("Song editor!");
+    }
+
+}]);
