4b8e400968c982891bb24fac5ec197a423a80a10
mgteutsch <mgt0117@gmail.com>
2017-06-19 20:46:15 -0500

    GET CustomLessons works and produces a list to user

--- Smorgaschord_Backend/App_xSmorgaschordx/Controllers/LessonsCtrl.js
+++ Smorgaschord_Backend/App_xSmorgaschordx/Controllers/LessonsCtrl.js
@@ -1,15 +1,16 @@
-﻿app.controller("LessonsCtrl", function ($q, $scope) {
+﻿app.controller("LessonsCtrl", ['$q', '$scope', '$rootScope','$http', '$location', function ($q, $scope, $rootScope, $http, $location) {
 
     //On page load, remove any styling done by Improviser
     angular.element(document.querySelectorAll('#overallBackground')).removeClass();
     angular.element(document.querySelectorAll('#navbar')).removeClass();
     angular.element(document.querySelectorAll('#navbar')).addClass("nav-wrapper pink accent-3");
 
-
+/* Collapsible isn't working with Visual Studio... handle later    
     //For More Details Collapsible:
     $(document).ready(function () {
         $('.collapsible').collapsible();
     });
+*/
 
 
     //Auto scroll to top
@@ -19,7 +20,6 @@
 
 
     //inner Navbar items:
-
     $scope.regularLessonsClick = function () {
         $scope.showHideLessons = false;
         angular.element(document.querySelector('#customLessonsTab')).removeClass();
@@ -36,4 +36,13 @@
         angular.element(document.querySelector('#customLessonsTab')).addClass("selected");
     };
 
-});
+    //GET list of Custom Lessons
+    let displayListOfCustomLessonContainers = function () {
+        $http.get('/api/CustomLessonTitle')
+            .then(function (dbLessonContainers) {
+                $scope.lessonListItems = dbLessonContainers.data;
+            });
+    }
+    displayListOfCustomLessonContainers();
+
+}]);
