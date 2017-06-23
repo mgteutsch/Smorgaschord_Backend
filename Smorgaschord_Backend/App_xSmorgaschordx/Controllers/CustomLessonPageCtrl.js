c4f746f0ba07924fcdd4ac7c283a5ea5a6c8c61c
mgteutsch <mgt0117@gmail.com>
2017-06-23 03:30:40 -0500

    Added placeholder chord animatios for custom lessons

--- Smorgaschord_Backend/App_xSmorgaschordx/Controllers/CustomLessonPageCtrl.js
+++ Smorgaschord_Backend/App_xSmorgaschordx/Controllers/CustomLessonPageCtrl.js
@@ -24,9 +24,53 @@
     }
     getLessonContent();
 
+
+
+
+    //Placeholder chord animations:
     
-    $scope.testAudioClick = function () {
-        console.log("YEP!");
+    let chord4PinkTexter = function(){
+        var nextChord;
+        let startNextChord = function(){
+            nextChord = setTimeout(function(){chord1PinkTexter();},1900);
+            angular.element(document.querySelector('.customChord3')).removeClass("pink-text");
+            angular.element(document.querySelector('.customChord4')).addClass("pink-text");
+        };
+        startNextChord();	
     }
+
+    let chord3PinkTexter = function(){
+        var nextChord;
+        let startNextChord = function(){
+            nextChord = setTimeout(function(){chord4PinkTexter();},1900);
+            angular.element(document.querySelector('.customChord2')).removeClass("pink-text");
+            angular.element(document.querySelector('.customChord3')).addClass("pink-text");
+        };
+        startNextChord();	
+    }
+
+    let chord2PinkTexter = function(){
+        var nextChord;
+        let startNextChord = function(){
+            nextChord = setTimeout(function(){chord3PinkTexter();},1900);
+            angular.element(document.querySelector('.customChord1')).removeClass("pink-text");
+            angular.element(document.querySelector('.customChord2')).addClass("pink-text");
+        };
+        startNextChord();	
+    }
+        
+
+    let chord1PinkTexter = function(){
+        var nextChord;
+        let startNextChord = function(){
+            nextChord = setTimeout(function(){chord2PinkTexter();},1900);
+            angular.element(document.querySelector('.customChord4')).removeClass("pink-text");
+            angular.element(document.querySelector('.customChord1')).addClass("pink-text");
+        };
+        startNextChord();	
+    }
+    
+    chord1PinkTexter();
+
 
 }]);
